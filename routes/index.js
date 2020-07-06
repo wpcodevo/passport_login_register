cc80cdfd84c9610c3510d436c9f28d12df304a67
Ziddah Edem <ziddah.edem@gmail.com>
2020-07-06 20:35:54 +0000

    :art: adding more route handlers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const {
+  ensureAuth,
+  forwardAuth
+} = require('./../config/auth')
 
 const router = express.Router();
 
 // WELCOME PAGE
-router.get('/', (req, res) => {
+router.get('/', forwardAuth, (req, res) => {
   res.status(200).render('welcome');
 });
+
+
+// DASHBOARD PAGE
+router.get('/dashboard', ensureAuth, (req, res) => {
+  res.status(200).render('dashboard', {
+    user: req.user
+  });
+  console.log(req.user)
+});
+
 module.exports = router;
