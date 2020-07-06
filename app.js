7de979033775d94477e153d2cf685d7f57594b05
Ziddah Edem <ziddah.edem@gmail.com>
2020-07-06 20:37:36 +0000

    :sparkles: updating the controller

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const expressLayouts = require('express-ejs-layouts')
+const flash = require('connect-flash')
+const session = require('express-session');
+const passport = require('passport');
+
+
+// PASSPORT CONFIG
+require('./config/passport')(passport)
 
 // ENV CONFIG
 dotenv.config({
@@ -18,9 +25,38 @@ if (process.env.NODE_ENV === 'development') {
 // DB CONFIG
 require('./config/db')();
 
+// PASSPORT MIDDLEWARE
+app.use(passport.initialize());
+app.use(passport.session());
+
+// SESSION
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+}))
+
+// CONNECT-FLASH
+app.use(flash())
+
+// GLOBAL VARS
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  res.locals.error = req.flash('error')
+  next()
+})
+
 // EJS
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
+
+// BODY PARSER
+app.use(express.urlencoded({
+  extended: false
+}))
+app.use(express.json())
+
 
 // ROUTES
 app.use('/', require('./routes/index'));
